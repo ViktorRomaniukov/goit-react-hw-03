c14cb08cd669962cfc135e96032fc8472a918b89
Viktor Romaniukov <viktorrvi@email.ua>
2025-04-10 17:57:24 +0300

    add hw-3

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contact from '../Contact/Contact';
+
+const ContactList = ({ contacts }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <Contact key={id} name={name} number={number} />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
